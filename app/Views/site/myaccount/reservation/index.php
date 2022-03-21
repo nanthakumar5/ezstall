f23d0f0e12a8c470862f5262c0223657b17938bc
HIRORAM <ramnathbacki@gmail.com>
2022-03-21 19:27:48 +0530

    Dashboard Reservation style update

--- app/Views/site/myaccount/reservation/index.php
+++ app/Views/site/myaccount/reservation/index.php
@@ -2,54 +2,39 @@
 <?php $this->section('content') ?>
 <h2 class="fw-bold mb-4">Current Reservation</h2>
 <section class="maxWidth eventPagePanel">
-<?php foreach ($bookings as $data) { ?>
-        <div class="dashboard-box">
-            <div class="EventFlex">
-                <div class="EventFlex leftdata">
-                      <span class="wi-30">
-                        <table>
-                          <tr class="mb-5">
-                            <td class="py-2 px-2"><h6 class="mb-0 fw-600">FitstName</h6></td>
-                            <td class="py-2 fw-600">:</td>
-                            <td class="py-2 px-2"><p class="mb-0"><?php echo $data['firstname'];?></p></td>
-                          </tr>
-                          <tr>
-                            <td class="py-2 px-2"><h6 class="mb-0 fw-600">LastName</h6></td>
-                            <td class="py-2 fw-600">:</td>
-                            <td class="py-2 px-2"><p class="mb-0"><?php echo $data['lastname'];?></p></td>
-                          </tr>
-                          <tr>
-                            <td class="py-2 px-2"><h6 class="mb-0 fw-600">Mobile</h6></td>
-                            <td class="py-2 fw-600">:</td>
-                            <td class="py-2 px-2"><p class="mb-0"> <?php echo $data['mobile'];?></p></td>
-                          </tr>
-                          <tr>
-                            <td class="py-2 px-2"><h6 class="mb-0 fw-600">Booked Event</h6></td>
-                            <td class="py-2 fw-600">:</td>
-                            <td class="py-2 px-2"><p class="mb-0"><?php echo $data['eventname'];?></p></td>
-                          </tr>
-
-                          <tr>
-                            <td class="py-2 px-2"><h6 class="mb-0 fw-600">Booked Stall</h6></td>
-                            <td class="py-2 fw-600">:</td>
-                            <td class="py-2 px-2"><p class="mb-0"><?php echo implode(',', $data['stall']);?></p></td>
-                          </tr>
-                          <tr>
-                            <td class="py-2 px-2"><h6 class="mb-0 fw-600">CheckIn</h6></td>
-                            <td class="py-2 fw-600">:</td>
-                            <td class="py-2 px-2"><p class="mb-0"><?php echo date("d-m-Y", strtotime($data['check_in']));?></p></td>
-                          </tr>
-                          <tr>
-                            <td class="py-2 px-2"><h6 class="mb-0 fw-600">CheckOut</h6></td>
-                            <td class="py-2 fw-600">:</td>
-                            <td class="py-2 px-2"><p class="mb-0"><?php echo date("d-m-Y", strtotime($data['check_out']));?></p></td>
-                          </tr>
-                        </table>
-                      </span>
-                </div>
+  <?php foreach ($bookings as $data) { ?>
+    <div class="dashboard-box">
+      <div class="EventFlex leftdata">
+        <div class="wi-30 row w-100">
+          <div class="col-md-4">
+            <div>
+              <h6 class="mb-0 fw-600">Name</h6></td>
+              <p class="mb-0"><?php echo $data['firstname'].$data['lastname'];?></p></td>
             </div>
+            <div class="my-2">
+              <h6 class="mb-0 fw-600">Booked Event</h6>
+              <p class="mb-0"><?php echo $data['eventname'];?> ( <?php echo implode(',', $data['stall']);?> )</p>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div>
+              <h6 class="mb-0 fw-600">CheckIn</h6>
+              <p class="mb-0"><?php echo date("d-m-Y", strtotime($data['check_in']));?></p>
+            </div>
+            <div class="d-flex">
+              <a href="#" class="view-res">View</a>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div>
+              <h6 class="mb-0 fw-600">CheckOut</h6>
+              <p class="mb-0"><?php echo date("d-m-Y", strtotime($data['check_out']));?></p>
+            </div>
+          </div>
         </div>
-     <?php } ?>
+      </div>
+    </div>
+  <?php } ?>
 
 </section>
 
