c4c430a802e9a212c9fc6fa5ae04f7cc90637796
HIRORAM <ramnathbacki@gmail.com>
2022-04-22 20:45:33 +0530

    Event page sticky panel design

--- app/Views/site/events/detail.php
+++ app/Views/site/events/detail.php
@@ -5,49 +5,50 @@ $userid 	= getSiteUserID() ? getSiteUserID() : 0;
 $getcart 	= getCart();
 $cartevent 	= ($getcart && $getcart['event_id'] != $detail['id']) ? 1 : 0;
 ?>
-<section class="maxWidth">
-	<div class="pageInfo">
-		<span class="marFive">
-			<a href="<?php echo base_url(); ?>">Home /</a>
-			<a href="<?php echo base_url().'/events'; ?>"> Events /</a>
-			<a href="javascript:void(0);"><?php echo $detail['name'] ?></a>
-		</span>
-	</div>
-	<?php if($cartevent==1){?>
-		<div class="alert alert-success alert-dismissible fade show m-2" role="alert">
-			For booking this event remove other event from the cart <a href="<?php echo base_url().'/events/detail/'.$getcart['event_id']; ?>">Go To Event</a>
-			<!--<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close">X</button>-->
-		</div>
-	<?php } ?>
-	<div class="marFive dFlexComBetween eventTP pb-3 pt-4">
-		<div class="pageInfo m-0 bg-transparent">
-			<span class="eventHead">
-				<a href="<?php echo base_url().'/events'; ?>" class="d-block"> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="14" fill="currentColor" class="bi bi-chevron-left" viewBox="0 0 16 16">
-					<path fill-rule="evenodd" d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"/>
-				</svg> Back To All Events</a>
+<body style="overflow: initial;">
+	<section class="maxWidth">
+		<div class="pageInfo">
+			<span class="marFive">
+				<a href="<?php echo base_url(); ?>">Home /</a>
+				<a href="<?php echo base_url().'/events'; ?>"> Events /</a>
+				<a href="javascript:void(0);"><?php echo $detail['name'] ?></a>
 			</span>
 		</div>
-	</div>
-</section>
-<section class="container-lg">
-	<div class="row">
-		<div class="col-md-12">
-			<div class="border rounded pt-5 ps-3 pe-3">
-				<div class="row">
-					<div class="col-6">
-						<span class="edimg">
-							<img src="<?php echo base_url() ?>/assets/uploads/event/<?php echo $detail['image']?>" width="350px" height="auto">
-						</span>
-					</div>
-					<div class="col-6">
-						<h4 class="checkout-fw-6"><?php echo $detail['name'] ?></h4>
-						<ul class="edaddr">
-							<li class="mb-3 mt-3">
-								<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-geo-alt-fill" viewBox="0 0 16 16">
-									<path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z"/>
-								</svg> 
-								<?php echo $detail['location'] ?>
-							</li>
+		<?php if($cartevent==1){?>
+			<div class="alert alert-success alert-dismissible fade show m-2" role="alert">
+				For booking this event remove other event from the cart <a href="<?php echo base_url().'/events/detail/'.$getcart['event_id']; ?>">Go To Event</a>
+				<!--<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close">X</button>-->
+			</div>
+		<?php } ?>
+		<div class="marFive dFlexComBetween eventTP pb-3 pt-4">
+			<div class="pageInfo m-0 bg-transparent">
+				<span class="eventHead">
+					<a href="<?php echo base_url().'/events'; ?>" class="d-block"> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="14" fill="currentColor" class="bi bi-chevron-left" viewBox="0 0 16 16">
+						<path fill-rule="evenodd" d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"/>
+					</svg> Back To All Events</a>
+				</span>
+			</div>
+		</div>
+	</section>
+	<section class="container-lg">
+		<div class="row" style="display: initial !important;">
+			<div class="col-md-12">
+				<div class="border rounded pt-5 ps-3 pe-3">
+					<div class="row">
+						<div class="col-6">
+							<span class="edimg">
+								<img src="<?php echo base_url() ?>/assets/uploads/event/<?php echo $detail['image']?>" width="350px" height="auto">
+							</span>
+						</div>
+						<div class="col-6">
+							<h4 class="checkout-fw-6"><?php echo $detail['name'] ?></h4>
+							<ul class="edaddr">
+								<li class="mb-3 mt-3">
+									<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-geo-alt-fill" viewBox="0 0 16 16">
+										<path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z"/>
+									</svg> 
+									<?php echo $detail['location'] ?>
+								</li>
 							<!-- <li class="mb-3">
 								<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-calendar4" viewBox="0 0 16 16">
 									<path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM2 2a1 1 0 0 0-1 1v1h14V3a1 1 0 0 0-1-1H2zm13 3H1v9a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V5z"/>
@@ -83,92 +84,95 @@ $cartevent 	= ($getcart && $getcart['event_id'] != $detail['id']) ? 1 : 0;
 					<span class="col-3">
 						<p class="mb-1 fw-bold"><img class="eventDIcon" src="<?php echo base_url() ?>/assets/site/img/date.png"> Start Date: </p>
 						<p class="ucDAte mb-0">
-						<?php echo formatdate($detail['start_date'], 1);?></p>
-					</span>
-					<span class="col-3 border-end">
-						<p class="mb-1 fw-bold"><img class="eventDIcon" src="<?php echo base_url() ?>/assets/site/img/date.png"> End Date: </p>
-						<p class="ucDAte mb-0"><?php echo formatdate($detail['end_date'], 1); ?></p>
-					</span>
-					<span class="col-3">
-						<p class="mb-1 fw-bold"><img class="eventDIcon" src="<?php echo base_url() ?>/assets/site/img/time.png"> Start Time: </p>
-						<p class="ucDAte mb-0"> after <?php echo formattime($detail['start_time']) ?></p>
-					</span>
-					<span class="col-3">
-						<p class="mb-1 fw-bold"><img class="eventDIcon" src="<?php echo base_url() ?>/assets/site/img/time.png"> End Time:</p>
-						<p class="ucDAte mb-0">by <?php echo formattime($detail['end_time']) ?></p>
-					</span>
+							<?php echo formatdate($detail['start_date'], 1);?></p>
+						</span>
+						<span class="col-3 border-end">
+							<p class="mb-1 fw-bold"><img class="eventDIcon" src="<?php echo base_url() ?>/assets/site/img/date.png"> End Date: </p>
+							<p class="ucDAte mb-0"><?php echo formatdate($detail['end_date'], 1); ?></p>
+						</span>
+						<span class="col-3">
+							<p class="mb-1 fw-bold"><img class="eventDIcon" src="<?php echo base_url() ?>/assets/site/img/time.png"> Start Time: </p>
+							<p class="ucDAte mb-0"> after <?php echo formattime($detail['start_time']) ?></p>
+						</span>
+						<span class="col-3">
+							<p class="mb-1 fw-bold"><img class="eventDIcon" src="<?php echo base_url() ?>/assets/site/img/time.png"> End Time:</p>
+							<p class="ucDAte mb-0">by <?php echo formattime($detail['end_time']) ?></p>
+						</span>
+					</div> 
+				</div>
+			</div>
+			<div class="row m-0 p-0">
+				<div class="col-md-9">
+					<div class="border rounded pt-4 ps-3 pe-3 mt-4 mb-5">
+						<h3 class="fw-bold mb-4">Book Your Stalls</h3>
+						<div class="infoPanel form_check">
+							<span class="infoSection">
+								<span class="iconProperty">
+									<input type="text" readonly id="stallcount"  value="0" placeholder="Number of Stalls">
+									<span class="num_btn stallcount"><button>+</button><br><button>-</button></span>
+								</span>
+								<span class="iconProperty">			
+									<input type="text" name="startdate" id="startdate" class ="checkdate checkin" autocomplete="off" placeholder="Check-In"/> 						
+								</span>
+								<span class="iconProperty">
+									<input type="text" name="enddate" id="enddate" class = "checkdate checkout" autocomplete="off"placeholder="Check-Out"/>
+								</span>
+							</span>
+						</div>
+
+						<?php 
+						$tabbtn = '';
+						$tabcontent = '';
+						foreach ($detail['barn'] as $barnkey => $barndata) {
+							$barnid = $barndata['id'];
+							$barnname = $barndata['name'];
+							$barnactive = $barnkey=='0' ? ' show active' : '';
+							$tabbtn .= '<button class="nav-link'.$barnactive.'" data-bs-toggle="tab" data-bs-target="#barn'.$barnid.'" type="button" role="tab" aria-controls="barn'.$barnid.'" aria-selected="true">'.$barnname.'</button>';
+
+							$tabcontent .= '<div class="tab-pane fade'.$barnactive.'" id="barn'.$barnid.'" role="tabpanel" aria-labelledby="nav-home-tab">
+							<ul class="list-group">';
+							foreach($barndata['stall'] as $stalldata){
+								$boxcolor  = 'green-box';
+								$checkboxstatus = '';
+
+								if($cartevent=='1' || $checkevent['status']=='0'){
+									$checkboxstatus = 'disabled';
+								}
+
+								$tabcontent .= 	'<li class="list-group-item">
+								<input class="form-check-input stallid me-1" data-price="'.$stalldata['price'].'" data-barnid="'.$stalldata['barn_id'].'" value="'.$stalldata['id'].'" name="checkbox"  type="checkbox" '.$checkboxstatus.'>
+								'.$stalldata['name'].'
+								<span class="'.$boxcolor.' stallavailability" data-stallid="'.$stalldata['id'].'" ></span>
+								</li>';
+							}
+							$tabcontent .= '</ul></div>';
+						}
+						?>
+
+						<div class="barn-nav mt-4">
+							<nav>
+								<div class="nav nav-tabs mb-4" id="nav-tab" role="tablist">
+									<?php echo $tabbtn; ?>
+								</div>
+							</nav>
+							<div class="tab-content" id="nav-tabContent">
+								<?php echo $tabcontent; ?>
+								<div class="row">
+									<div class="btm-color">
+										<p><span class="green-circle"></span>Available</p>
+										<p><span class="yellow-circle"></span>Reserved</p>
+										<p><span class="red-circle"></span>Occupied</p>
+									</div>
+								</div>
+							</div>    
+						</div>
+					</div>
 				</div> 
+				<div class="sticky-top checkout col-md-3 mt-4 h-100"></div>
 			</div>
 		</div>
-		<div class="col-md-9">
-			<div class="border rounded pt-4 ps-3 pe-3 mt-4 mb-5">
-				<h3 class="fw-bold mb-4">Book Your Stalls</h3>
-				<div class="infoPanel form_check">
-					<span class="infoSection">
-						<span class="iconProperty">
-							<input type="text" readonly id="stallcount"  value="0" placeholder="Number of Stalls">
-							<span class="num_btn stallcount"><button>+</button><br><button>-</button></span>
-						</span>
-						<span class="iconProperty">			
-							<input type="text" name="startdate" id="startdate" class ="checkdate checkin" autocomplete="off" placeholder="Check-In"/> 						
-						</span>
-						<span class="iconProperty">
-							<input type="text" name="enddate" id="enddate" class = "checkdate checkout" autocomplete="off"placeholder="Check-Out"/>
-						</span>
-					</span>
-				</div>
-
-				<?php 
-				$tabbtn = '';
-				$tabcontent = '';
-				foreach ($detail['barn'] as $barnkey => $barndata) {
-					$barnid = $barndata['id'];
-					$barnname = $barndata['name'];
-					$barnactive = $barnkey=='0' ? ' show active' : '';
-					$tabbtn .= '<button class="nav-link'.$barnactive.'" data-bs-toggle="tab" data-bs-target="#barn'.$barnid.'" type="button" role="tab" aria-controls="barn'.$barnid.'" aria-selected="true">'.$barnname.'</button>';
-
-					$tabcontent .= '<div class="tab-pane fade'.$barnactive.'" id="barn'.$barnid.'" role="tabpanel" aria-labelledby="nav-home-tab">
-					<ul class="list-group">';
-					foreach($barndata['stall'] as $stalldata){
-						$boxcolor  = 'green-box';
-						$checkboxstatus = '';
-						
-						if($cartevent=='1' || $checkevent['status']=='0'){
-							$checkboxstatus = 'disabled';
-						}
-						
-						$tabcontent .= 	'<li class="list-group-item">
-						<input class="form-check-input stallid me-1" data-price="'.$stalldata['price'].'" data-barnid="'.$stalldata['barn_id'].'" value="'.$stalldata['id'].'" name="checkbox"  type="checkbox" '.$checkboxstatus.'>
-						'.$stalldata['name'].'
-						<span class="'.$boxcolor.' stallavailability" data-stallid="'.$stalldata['id'].'" ></span>
-						</li>';
-					}
-					$tabcontent .= '</ul></div>';
-				}
-				?>
-
-				<div class="barn-nav mt-4">
-					<nav>
-						<div class="nav nav-tabs mb-4" id="nav-tab" role="tablist">
-							<?php echo $tabbtn; ?>
-						</div>
-					</nav>
-					<div class="tab-content" id="nav-tabContent">
-						<?php echo $tabcontent; ?>
-						<div class="row">
-							<div class="btm-color">
-								<p><span class="green-circle"></span>Available</p>
-								<p><span class="yellow-circle"></span>Reserved</p>
-								<p><span class="red-circle"></span>Occupied</p>
-							</div>
-						</div>
-					</div>    
-				</div>
-			</div>
-		</div> 
-		<div class="checkout col-md-3 mt-4"></div>
-	</div>
-</section>
+	</section>
+</body>
 <?php $this->endSection() ?>
 
 <?php $this->section('js') ?>
@@ -202,7 +206,7 @@ $cartevent 	= ($getcart && $getcart['event_id'] != $detail['id']) ? 1 : 0;
 					$("#enddate").datepicker( "option", "minDate", date );
 				}
 			}
-		);
+			);
 
 		uidatepicker('#enddate', { 'mindate' : eventstartdate, 'maxdate' : eventenddateadd });
 	});
@@ -241,7 +245,7 @@ $cartevent 	= ($getcart && $getcart['event_id'] != $detail['id']) ? 1 : 0;
 					});
 				}
 			}
-		)
+			)
 		
 		ajax(
 			'<?php echo base_url()."/ajax/ajaxreserved"; ?>',
@@ -255,7 +259,7 @@ $cartevent 	= ($getcart && $getcart['event_id'] != $detail['id']) ? 1 : 0;
 					});
 				}
 			}
-		)
+			)
 	}
 	
 	$(".form-check-input").on("click", function() {
@@ -316,28 +320,28 @@ $cartevent 	= ($getcart && $getcart['event_id'] != $detail['id']) ? 1 : 0;
 						var total = (result.price+8.50);
 						var result ='\
 						<div class="w-100">\
-							<div class="border rounded pt-4 ps-3 pe-3 mb-5">\
-								<div class="row mb-2">\
-									<div class="col-md-8 ">\
-										<span>'+result.barnstall.length+'</span> Stalls x \
-										<span>'+result.interval+'</span> Nights \
-									</div>\
-									<div class="col-4">\
-									$'+result.price+'\
-									</div>\
-								</div>\
-								<div class="row mb-2">\
-									<div class="col-8 ">Transaction Fees</div>\
-									<div class="col-4">$8.50</div>\
-								</div>\
-								<div class="row mb-2 border-top mt-3 mb-3 pt-3">\
-									<div class="col-8 fw-bold ">Total Due</div>\
-									<div class="col-4 fw-bold">$'+total+'</div>\
-								</div>\
-								<div class="row mb-2 w-100">\
-									<a href="<?php echo base_url()?>/checkout" class="w-100 text-center mx-2 ucEventdetBtn ps-3 mb-3 ">Continue to Checkout</a>\
-								</div>\
-							</div>\
+						<div class="border rounded pt-4 ps-3 pe-3 mb-5">\
+						<div class="row mb-2">\
+						<div class="col-md-8 ">\
+						<span>'+result.barnstall.length+'</span> Stalls x \
+						<span>'+result.interval+'</span> Nights \
+						</div>\
+						<div class="col-4">\
+						$'+result.price+'\
+						</div>\
+						</div>\
+						<div class="row mb-2">\
+						<div class="col-8 ">Transaction Fees</div>\
+						<div class="col-4">$8.50</div>\
+						</div>\
+						<div class="row mb-2 border-top mt-3 mb-3 pt-3">\
+						<div class="col-8 fw-bold ">Total Due</div>\
+						<div class="col-4 fw-bold">$'+total+'</div>\
+						</div>\
+						<div class="row mb-2 w-100">\
+						<a href="<?php echo base_url()?>/checkout" class="w-100 text-center mx-2 ucEventdetBtn ps-3 mb-3 ">Continue to Checkout</a>\
+						</div>\
+						</div>\
 						</div>\
 						';
 
@@ -348,7 +352,7 @@ $cartevent 	= ($getcart && $getcart['event_id'] != $detail['id']) ? 1 : 0;
 					}
 				}
 			}
-		);
+			);
 	}
 </script>
 <?php echo $this->endSection() ?>
