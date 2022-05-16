62f558a52562f917d65ca9b99d5ccc9d28b14afa
muthulakshmi <muthulakshmi@itflexsolutions.com>
2022-05-16 16:00:03 +0530

    custom.php

--- app/Config/Custom.php
+++ app/Config/Custom.php
@@ -8,7 +8,7 @@ class Custom extends BaseConfig
 {
 	public $usertype    		= ['1'=>'Admin','2'=>'Facility','3'=>'Producer','4'=>'Stall Manager','5'=>'Horse Owner'];
 	public $status1  			= ['1'=>'Enable','2'=>'Disable'];
-	public $paymentinterval 	= ['week' => 'Weekly Subscription', 'month' => 'Monthly Subscription', 'year' => 'Yearly Subscription'];
+	public $paymentinterval 	= [/*'week' => 'Weekly Subscription',*/ 'month' => 'Monthly Subscription', 'year' => 'Yearly Subscription'];
 	public $paymenttype 		= ['1' => 'Payment', '2' => 'Subscription'];
 	public $paymentuser 		= ['2' => 'Facility', '5' => 'Horse Owner'];
 	public $paymentstatus 		= ['1' => 'Paid', '2' => 'Refunded'];
